62a3ec85758e2a0725da121e08f1cbdb989daeea
phyranja <phyranja@charon.ch>
2016-04-13 18:14:48 +0200

    Added startmenu to the project
    Simple startmenu, can load the Basic Scene (Level1) and quit the game
    (with doublecheck).

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button play1;
+	public Button play2;
+	public Button quit;
+
+	// Use this for initialization
+	void Start () {
+
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		quitMenu.enabled = false;
+		play1 = play1.GetComponent<Button> ();
+		play2 = play2.GetComponent<Button> ();
+		quit = quit.GetComponent<Button> ();
+
+	}
+
+	public void OnQuitClick(){
+		play1.enabled = false;
+		play2.enabled = false;
+		quit.enabled = false;
+		quitMenu.enabled = true;
+
+	}
+
+	public void OnQuitAbort(){
+		play1.enabled = true;
+		play2.enabled = true;
+		quit.enabled = true;
+		quitMenu.enabled = false;
+
+	}
+
+	public void OnPlay1(){
+		SceneManager.LoadScene("BasicScene");
+	}
+
+	public void OnQuitConfirm(){
+		Application.Quit ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
