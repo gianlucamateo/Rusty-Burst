cb07c967b06560610c6d06027e274bafa10729c2
gianlucamateo <gianluca.mateo@gmail.com>
2016-05-13 11:19:13 +0200

    Added air control, Ice tyres, Brake lights
    Yaw/pitch control, ice tyres via flag on chassis

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -18,17 +18,36 @@ public class CarController : MonoBehaviour {
 	public Vector3 worldPose;
 	private float slip;
 	public float skidSlip;
+	public List<GameObject> brakeLights;
+	public List<GameObject> tyres;
+	private bool inAir;
+	public bool iceTyres = false;
+	private Color tyreBaseColor;
+	private WheelFrictionCurve frontBaseSide,frontBaseForward, rearBaseSide, rearBaseForward;
+
 
 	public void Start(){
 		axleInfos[0].leftWheel.gameObject.GetComponent<WheelCollider>().ConfigureVehicleSubsteps(5f, 50, 50);
+		this.tyreBaseColor = tyres [0].GetComponent<Renderer> ().material.GetColor ("_Color");
+		this.frontBaseForward = axleInfos [1].leftWheel.forwardFriction;
+		this.frontBaseSide = axleInfos [1].leftWheel.sidewaysFriction;
+
+		this.rearBaseForward = axleInfos [0].leftWheel.forwardFriction;
+		this.rearBaseSide = axleInfos [0].leftWheel.sidewaysFriction;
 	}
+
 
 
 	public void FixedUpdate()
 	{
+		float forwards = Input.GetAxis ("Triggers") - Input.GetAxis ("Vertical");
+		float steeringInput = Input.GetAxis ("Horizontal");
+		inAir = !Physics.Raycast (this.transform.position, -this.transform.up, 1f);
 		input = Input.GetAxis ("Triggers");
-		float motor = maxMotorTorque * (Input.GetAxis("Triggers")-Input.GetAxis("Vertical"));
-		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		float motor = maxMotorTorque * forwards;
+		float steering = maxSteeringAngle * steeringInput;
+
+		handleIceTyres (iceTyres);
 
 		foreach (AxleInfo axleInfo in axleInfos) {
 			if (axleInfo.steering) {
@@ -41,16 +60,26 @@ public class CarController : MonoBehaviour {
 					axleInfo.rightWheel.brakeTorque = motor * axleInfo.brakeScale ;
 					axleInfo.leftWheel.motorTorque = 0;
 					axleInfo.rightWheel.motorTorque = 0;
+					foreach (GameObject brakeLight in brakeLights) {
+						brakeLight.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.red);
+					}
 				} else {
 					axleInfo.leftWheel.brakeTorque = 0;
 					axleInfo.rightWheel.brakeTorque = 0;
 					axleInfo.leftWheel.motorTorque = -motor * axleInfo.motorScale;
 					axleInfo.rightWheel.motorTorque = -motor* axleInfo.motorScale;
+					foreach (GameObject brakeLight in brakeLights) {
+						brakeLight.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.black);
+					}
 				}
 
 			}
-
-			checkSkids (axleInfo);
+			if (inAir) {
+				moveAir(forwards,steeringInput);
+			}
+			if (!iceTyres) {
+				checkSkids (axleInfo);
+			}
 
 		}
 
@@ -63,6 +92,51 @@ public class CarController : MonoBehaviour {
 		chassis.drag = ratio * ratio * 0.3f;
 
 	}
+
+	private void handleIceTyres(bool iceTyres){
+		if (iceTyres) {
+			foreach (AxleInfo axleInfo in axleInfos) {
+				WheelFrictionCurve curve = axleInfo.leftWheel.forwardFriction;
+				curve.stiffness = 0.5f;
+				axleInfo.leftWheel.forwardFriction = curve;
+				curve = axleInfo.rightWheel.forwardFriction;
+				curve.stiffness = 0.5f;
+				axleInfo.rightWheel.forwardFriction = curve;
+
+				curve = axleInfo.leftWheel.sidewaysFriction;
+				curve.stiffness = 0.5f;
+				axleInfo.leftWheel.sidewaysFriction = curve;
+				curve = axleInfo.rightWheel.sidewaysFriction;
+				curve.stiffness = 0.5f;
+				axleInfo.rightWheel.sidewaysFriction = curve;
+			}
+
+			foreach (GameObject tyre in tyres) {
+				tyre.GetComponent<Renderer> ().material.SetColor ("_Color", new Color (0.75f, 0.95f, 1f));
+			}
+		} else {
+			axleInfos [1].leftWheel.forwardFriction = this.frontBaseForward;
+			axleInfos [1].leftWheel.sidewaysFriction = this.frontBaseSide;
+
+			axleInfos [1].rightWheel.forwardFriction = this.frontBaseForward;
+			axleInfos [1].rightWheel.sidewaysFriction = this.frontBaseSide;
+
+			axleInfos [0].leftWheel.forwardFriction = this.rearBaseForward;
+			axleInfos [0].leftWheel.sidewaysFriction = this.rearBaseSide;	
+
+			axleInfos [0].rightWheel.forwardFriction = this.rearBaseForward;
+			axleInfos [0].rightWheel.sidewaysFriction = this.rearBaseSide;
+
+			foreach (GameObject tyre in tyres) {
+				tyre.GetComponent<Renderer> ().material.SetColor ("_Color", this.tyreBaseColor);
+			}
+		}
+	}
+
+	private void moveAir(float forward, float steering){
+		chassis.AddRelativeTorque (-forward*300,steering*300,0);
+	}
+
 	private void checkSkids(AxleInfo axleInfo){
 		worldPose = new Vector3 ();
 		Quaternion quat = new Quaternion ();
