b6af4874b70c6c1e29c070d73f3f38bf1c04855f
Nina Mujkanovic <nina.mujkanovic@students.unibe.ch>
2016-05-26 10:22:52 +0200

    destroy bullet after x seconds

---
+++ Assets/DestroyBullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBullet : MonoBehaviour {
+
+	// Change time to destroy bullets here
+	float destructTime = 5.0f;
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy(gameObject, destructTime);
+	}
+}
