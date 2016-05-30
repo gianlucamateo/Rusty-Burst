50040bcd86a4bb2a50f1d35f04ae9422ba92dd9b
Urs Gerber <ug.gerber@gmail.com>
2016-05-30 14:11:47 +0200

    Add checkpoint triggers to track

---
+++ Assets/TrackManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrackManager : MonoBehaviour {
+
+	public List<GameObject> checkpoints = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGui() {
+		GUI.DrawTexture (new Rect (0, 0, 20, 20), new Texture ());
+	}
+}
