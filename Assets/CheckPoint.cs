4acf57d068ee541a860ada157c757b14b9140201
Urs Gerber <ug.gerber@gmail.com>
2016-06-02 15:04:34 +0200

    Add rank calculation, chassis is now root object

--- Assets/CheckPoint.cs
+++ Assets/CheckPoint.cs
@@ -18,12 +18,8 @@ public class CheckPoint : MonoBehaviour {
 
 	// Report player entering checkpoint to Manager
 	void OnTriggerEnter(Collider other) {
-		if (IsPlayer(other))
-			manager.NotifyCheckpoint (other.gameObject.transform.root.gameObject, index);
-	}
-
-	bool IsPlayer(Collider collider) {
-		var car = collider.gameObject.transform.root.gameObject;
-		return car == manager.player1 || car == manager.player2;
+		var player = other.gameObject.GetComponentInParent<Player> ();
+		if (player != null)
+			manager.NotifyCheckpoint (player, index);
 	}
 }
--- Assets/TrackManager.cs
+++ Assets/TrackManager.cs
@@ -10,39 +10,65 @@ public class TrackManager : MonoBehaviour {
 
 	public List<GameObject> checkpoints = new List<GameObject>();
 
-	public GameObject player1;
-	public GameObject player2;
+	public Player player1, player2;
 
-	private GameObject player1Body, player2Body;
-
-    private int[] nextCheckPoint = {0, 0};
+	private int[] nextCheckPoint = { 0, 0 };
     private int[] round = {0, 0};
+	public int[] rank = { 0, 0 };
+
+	public float trackLength = 0.0f;
+	public float[] distanceToFinish = { 0f, 0f };
+
 
 	void Start () {
-		// Because "chassis" is the GO with the scripts attached, not the root "car" object :/)
-		player1Body = player1.transform.GetChild (0).gameObject;
-		player2Body = player2.transform.GetChild (0).gameObject;
-
         // Teleport to start
-	    AlignAtCheckPoint(0, player1Body, RESET_SEPARATION_DISTANCE);
-        AlignAtCheckPoint(0, player2Body, -RESET_SEPARATION_DISTANCE);
+	    AlignAtCheckPoint(0, player1, RESET_SEPARATION_DISTANCE);
+        AlignAtCheckPoint(0, player2, -RESET_SEPARATION_DISTANCE);
+
+		// Compute track length
+		for (var i = 0; i < checkpoints.Count - 1; i++) {
+			trackLength += (checkpoints [i + 1].transform.position - checkpoints [i].transform.position).magnitude;
+		}
 	}
 
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Period))
-	        ResetPlayer(player1Body, RESET_SEPARATION_DISTANCE);
+	    if (Input.GetKeyDown(KeyCode.R))
+	        ResetPlayer(player1, RESET_SEPARATION_DISTANCE);
 
-	    if (Input.GetKeyDown(KeyCode.Period))
-	        ResetPlayer(player2Body, -RESET_SEPARATION_DISTANCE);
+		if (Input.GetKeyDown(KeyCode.Period))
+	        ResetPlayer(player2, -RESET_SEPARATION_DISTANCE);
+
+		// Calculate Player Rank (num rounds + distance to finish)
+		distanceToFinish[0] = (player1.transform.position - checkpoints[nextCheckPoint[0]].transform.position).magnitude;
+		distanceToFinish[1] = (player2.transform.position - checkpoints[nextCheckPoint[1]].transform.position).magnitude;
+
+		for (var i = nextCheckPoint [0]; i < checkpoints.Count; i++)
+			distanceToFinish [0] += (checkpoints [i].transform.position - checkpoints [(i + 1) % checkpoints.Count].transform.position).magnitude;
+		for (var i = nextCheckPoint [1]; i < checkpoints.Count; i++)
+			distanceToFinish [1] += (checkpoints [i].transform.position - checkpoints [(i + 1) % checkpoints.Count].transform.position).magnitude;
+
+		var score = new int[2] { round [0] * 10, round [1] * 10 };
+		score [0] += distanceToFinish [0] < distanceToFinish [1] ? 1 : 0;
+		score [1] += distanceToFinish [1] < distanceToFinish [0] ? 1 : 0;
+
+		if (score [0] > score [1]) {
+			rank [0] = 1;
+			rank [1] = 2;
+		}
+		else{
+			rank [0] = 2;
+			rank [1] = 1;
+		}
+
 	}
 
 	void OnGUI() {
-        GUI.Label(new Rect(0, 0, 200, 20), String.Format("Player 0: Next CP: {0}, Round: {1}", nextCheckPoint[0], round[0]));
-        GUI.Label(new Rect(0, 20, 200, 20), String.Format("Player 1: Next CP: {0}, Round: {1}", nextCheckPoint[1], round[1]));
+		GUI.Label(new Rect(0, 0, 400, 20), String.Format("Player 0: Next CP: {0}, Round: {1}, Rank: {2}", nextCheckPoint[0], round[0], rank[0]));
+		GUI.Label(new Rect(0, 20, 400, 20), String.Format("Player 1: Next CP: {0}, Round: {1}, Rank: {2}", nextCheckPoint[1], round[1], rank[1]));
     }
 
-	public void NotifyCheckpoint(GameObject player, int checkpoint) {
-	    int playerId = GetPlayerId(player);
+	public void NotifyCheckpoint(Player player, int checkpoint) {
+		int playerId = player.playerId;
 
         Debug.Log(String.Format("Player {0} entered checkpoint {1}", playerId, checkpoint));
 
@@ -52,32 +78,27 @@ public class TrackManager : MonoBehaviour {
             // Check for completed round
             if (nextCheckPoint[playerId] == 1) {
                 round[playerId]++;
+				distanceToFinish [playerId] = 0.0f;
             }
 	    }
 	}
 
-	private void ResetPlayer (GameObject player, float offset) {
-	    int playerId = GetPlayerId(player);
-
-	    int previousCP = nextCheckPoint[playerId] - 1;
+	private void ResetPlayer (Player player, float offset) {
+		int previousCP = nextCheckPoint[player.playerId] - 1;
 	    if (previousCP < 0) previousCP = 0;
 
         // Reset Position
         AlignAtCheckPoint(previousCP, player, offset);
 		// Reset physical properties
-		player.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
-		player.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+		player.GetComponent<Rigidbody>().velocity = Vector3.zero;
 	}
 
-    private void AlignAtCheckPoint(int checkpoint, GameObject player, float offset = 0f) {
+    private void AlignAtCheckPoint(int checkpoint, Player player, float offset = 0f) {
         var cp = checkpoints[checkpoint];
+		var go = player.gameObject;
 
-        player.transform.position = cp.transform.position + new Vector3(offset, RESET_HEIGHT_ABOVE_TRACK, 2f);
-        player.transform.rotation = cp.transform.localRotation;
+		go.transform.position = cp.transform.position + new Vector3(offset, RESET_HEIGHT_ABOVE_TRACK, 2f);
+		go.transform.rotation = cp.transform.localRotation;
     }
-
-    private int GetPlayerId(GameObject player) {
-        return player == player1 ? 0 : 1;
-    }
-
 }
