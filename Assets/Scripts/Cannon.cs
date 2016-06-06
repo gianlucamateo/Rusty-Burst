02791c11ae29ea8d5bc94056b77f17872d423736
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2016-06-06 18:41:17 +0200

    added indicator for special bullets

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -10,10 +10,13 @@ public class Cannon : MonoBehaviour {
 	public Animation fireAnim;
 	public KeyCode up, down, left, right, fireKey;
 	public Texture2D reticle;
+	public Texture2D ice, fat, engine, empty;
 	public Camera carCam;
 	public Rect carVP;
-	public Vector3 basePos, posOnScreen;
+	public Vector3 basePos, posOnScreen, iconPosOnScreen;
 	public CarController carCtrl;
+
+	private Dictionary<Bullet.Type,Texture2D> imgDict;
 
 	private bool fire = true;
 
@@ -62,6 +65,25 @@ public class Cannon : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		ice = new Texture2D (1024,1024);
+		ice.LoadImage (System.IO.File.ReadAllBytes("Assets/textures/IceBullet.png"));
+
+		fat = new Texture2D (1024,1024);
+		fat.LoadImage (System.IO.File.ReadAllBytes("Assets/textures/fatty.png"));
+
+		engine = new Texture2D (1024,1024);
+		engine.LoadImage (System.IO.File.ReadAllBytes("Assets/textures/EngineStunBullet.png"));
+
+		empty = new Texture2D (1024,1024);
+		empty.LoadImage (System.IO.File.ReadAllBytes("Assets/textures/empty.png"));
+
+		imgDict = new Dictionary<Bullet.Type, Texture2D>(){
+			{Bullet.Type.NORMAL, empty},
+			{Bullet.Type.HEAVY, fat},
+			{Bullet.Type.ICE, ice},
+			{Bullet.Type.ENGINE_STUN, engine},
+		};
+
 		carCtrl = transform.gameObject.GetComponentInParent<CarController> ();
 	}
 	
@@ -82,11 +104,14 @@ public class Cannon : MonoBehaviour {
 
 		posOnScreen = carCam.WorldToScreenPoint(transform.position + 0.2f*transform.up + 1000f*transform.forward)- new Vector3(RETICLE_SIZE / 2f,-RETICLE_SIZE / 2f,0f);
 
+		iconPosOnScreen = posOnScreen + new Vector3(RETICLE_SIZE*0.8f,-RETICLE_SIZE*0.8f);
+
 		if (rayCast) {
 			posOnScreen = carCam.WorldToScreenPoint (rcHit.point) - new Vector3(RETICLE_SIZE / 2f, -RETICLE_SIZE / 2f ,0f);
 		}
 
 		GUI.DrawTexture (new Rect (posOnScreen.x, Screen.height - posOnScreen.y, RETICLE_SIZE, RETICLE_SIZE), reticle);
+		GUI.DrawTexture (new Rect(iconPosOnScreen.x,Screen.height - iconPosOnScreen.y,30,30),imgDict[carCtrl.modifier]);
 	}
 
 	IEnumerator Fire(){
